5a201ddfa6c5f37aafbe6a86598e83542d97b644
YoharJB <yoharjb@gmail.com>
2025-01-17 23:13:49 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://pentsec.github.io/',
+    site: 'https://YoharJB.github.io/',
     base: '/Projects'
 })
